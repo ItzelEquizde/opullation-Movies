e6822ea385b0ae7a3c10e5b769d71ccd939377eb
ItzelEquizde <86081169+ItzelEquizde@users.noreply.github.com>
2023-03-10 22:56:14 -0600

    Add files via upload

---
+++ src/assets/properties/properties.ts
@@ -0,0 +1,4 @@
+export const properties = {
+  logo:'https://www.google.com.mx/url?sa=i&url=https%3A%2F%2Fwww.elespanol.com%2Fomicrono%2Fsoftware%2F20170825%2Funica-cuenta-acceder-organismos-publicos-login-gov%2F241726881_0.html&psig=AOvVaw35YNVQq6S4WJRKHUNHBI3H&ust=1677677524477000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCNiFuKqquP0CFQAAAAAdAAAAABAD'
+}
+
